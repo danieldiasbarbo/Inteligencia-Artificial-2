92a63d4074defe4ae3bb5c105029ccb625121378
Daniel Dias Barbosa <danieldiasbarbo@gmail.com>
2023-11-19 16:10:41 -0300

    Finalizado

--- README.md
+++ README.md
@@ -1,37 +1,33 @@
 # Projeto de Classificação
 
-O projeto consiste em uma análise comparativa de diferentes métodos de classificação usados para predizer a próxima jogada de um jogador de poker, utilizando os parâmetros de acurácia, tempo de treino e tempo de predição. Os métodos avaliados são: KNN, SVC, Árvore de Decisão, MLP e Naive Bayes.
+O projeto consiste em uma análise comparativa de diferentes métodos de classificação usados para classificar espécies de flor, utilizando os parâmetros de acurácia, sensitividade, especificidade, precisão tempo de treino e tempo de predição. Os métodos avaliados são: KNN, SVC, Árvore de Decisão, MLP e Naive Bayes.
 
-Repositório disponível em: https://github.com/danieldiasbarbo/Data-Mining
+Repositório disponível em: https://github.com/danieldiasbarbo/Inteligencia-Artificial-2
 
 ## Dependências
 
-Certifique-se de ter as seguintes dependências instaladas:
+Crie um ambiente virtual executando o seguinte comando:
 
-- pandas
-- scikit-learn
-- tabulate
+      python -m venv venv
 
-Você pode instalá-las executando o seguinte comando:
-```
-pip install pandas scikit-learn tabulate
-```
+- Ative o ambiente usando:
+
+      ./venv/Scripts/activate
+      
+- Instale as dependências usando:
+
+      pip install -r requirements.txt
+        
+
 
 ## Executando o projeto
 
-Siga as etapas abaixo para executar o projeto:
+Execute o comando:
 
-1. Faça o download ou clone o repositório do projeto em seu ambiente local.
-2. Certifique-se de ter a versão mais atualizada do Python instalado em seu sistema.
-3. Instale as dependências mencionadas acima.
-4. Abra o terminal ou prompt de comando e navegue até o diretório do projeto dentro da pasta source.
-5. Execute o seguinte comando para iniciar a execução do script:
 ```
 python main.py
 ```
 
 O script `main.py` executará os algoritmos de classificação nos dados fornecidos e exibirá os resultados em forma de tabela.
-
-Certifique-se de que os arquivos de dados necessários estejam presentes no diretório do projeto, ou atualize o script `main.py` para carregar os dados corretos de acordo com o seu caso de uso.
 
 Em caso de erro, confira se as dependências estão instaladas e se a versão correta do Python está instalada. A versão usada no desenvolvimento foi Python 3.11.3
--- main.py
+++ main.py
@@ -6,36 +6,25 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import f1_score
+from sklearn.metrics import classification_report
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neural_network import MLPClassifier
 from sklearn.naive_bayes import GaussianNB
 
-path = "../data/ZP-River.dat"
-quantidade_teste = 10
+path = "./data/iris.data"
+quantidade_teste = 100
 
 
 def load_data(path):
     diretorio = os.path.join(os.path.dirname(__file__), path)
 
-    nomes = [
-        "POSITION",
-        "EHS",
-        "TOTAL_POT",
-        "POT_ODDS",
-        "BOARD_SUIT",
-        "BOARD_CARDS",
-        "BOARD_CONNECT",
-        "PREV_ROUND_ACTION",
-        "PREVIOUS_ACTION",
-        "BET_VILLAIN",
-        "AGG",
-        "IP_VS",
-        "OOP_VS",
-        "ACTION_HERO",
-    ]
+    nomes = ["SEPAL_LENGHT", "SEPAL_WIDTH", "PETAL_LENGHT", "PETAL_WIDTH", "CLASS"]
 
     return pd.read_table(
-        filepath_or_buffer=diretorio, header=None, names=nomes, sep=" "
+        filepath_or_buffer=diretorio, header=None, names=nomes, sep=","
     )
 
 
@@ -47,14 +36,17 @@ def classificar(i_tr, i_te, o_tr, o_te, metodos):
     predicao = metodos[1].predict(i_te)
     fim_predicao = time.time()
     tempo_predicao = fim_predicao - fim_treino
-    final = accuracy_score(o_te, predicao)
-    return [final, tempo_treino, tempo_predicao]
+    acuracia = accuracy_score(o_te, predicao)
+    precisao = precision_score(o_te, predicao, average="macro", zero_division=0)
+    recall = recall_score(o_te, predicao, average="macro")
+    f1 = f1_score(o_te, predicao, average="macro")
+    return [acuracia, precisao, recall, f1, tempo_treino, tempo_predicao]
 
 
 def benchmark(metodos, proporcao):
     dados = load_data(path)
-    input = dados.drop("ACTION_HERO", axis=1)
-    output = dados.ACTION_HERO
+    input = dados.drop("CLASS", axis=1)
+    output = dados.CLASS
     input_treino, input_teste, output_treino, output_teste = train_test_split(
         input, output, train_size=proporcao
     )
@@ -92,12 +84,20 @@ if __name__ == "__main__":
         res = benchmark(metodos, 2 / 3)
         for chave in res:
             temp = list(map(lambda x: x / quantidade_teste, res[chave]))
-            f = final.get(chave, [0, 0, 0])
+            f = final.get(chave, [0, 0, 0, 0, 0, 0])
             final[chave] = list(map(lambda x: x[0] + x[1], zip(temp, f)))
 
     tabela = []
     for chave, valores in final.items():
         tabela.append([chave] + valores)
 
-    headers = ["Nome método", "Acurácia", "Tempo Treino", "Tempo Predição"]
+    headers = [
+        "Nome método",
+        "Acurácia",
+        "Precisao",
+        "recall",
+        "F1",
+        "Tempo Treino",
+        "Tempo Predição",
+    ]
     print(tabulate(tabela, headers=headers, tablefmt="grid"))
